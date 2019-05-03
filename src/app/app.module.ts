c471f2dc910ff9cd033ab2059681e3bf9d0476b4
ssubedir <ssubedir>
2019-05-03 19:23:42 -0400

    GET, POST working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SandboxComponent } from './components/sandbox/sandbox.component';
+import { RequestComponent } from './components/request/request.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SandboxComponent,
+    RequestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/request/request.component.ts
@@ -0,0 +1,112 @@
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClientModule, HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, Operator } from 'rxjs';
+import { Config } from 'protractor';
+
+@Component({
+  selector: 'app-request',
+  templateUrl: './request.component.html',
+  styleUrls: ['./request.component.css']
+})
+
+@Injectable()
+export class RequestComponent implements OnInit {
+
+  http:HttpClient;
+  input:string;
+  method:string;
+  path:string;
+  data: string;
+  response:any;
+
+  constructor(private _http:HttpClient) {
+    this.http = _http;
+   }
+
+  ngOnInit() {
+  }
+
+  _setup(){
+    
+    this.method = (document.getElementById('gprequest')  as HTMLSelectElement).value;
+    this.path = (document.getElementById('input-path')  as HTMLSelectElement).value;
+    this.data = (document.getElementById('data-input') as HTMLSelectElement).value;
+
+    
+    if(this.method == "GET"){
+      // get request not empty
+      try{
+        let obs =  this.http.get(this.path);
+        obs.subscribe(
+          data => {
+            //this.response; console.log(JSON.stringify(data));
+            (document.getElementById('http-response')  as HTMLSelectElement).value = JSON.stringify(data, null, 2)
+          },
+          error =>{
+            //this.response; console.log(JSON.stringify(error));
+            (document.getElementById('http-response')  as HTMLSelectElement).value = JSON.stringify(error, null, 2)
+
+          },
+        );
+
+      }
+      catch(error){
+        console.log(error);
+      }
+
+    } 
+    else if(this.method == "POST"){
+      // post request
+
+      try{
+      //   let obs1 = this.http.post(this.path,this.data)
+      //   .toPromise().then(response => {(document.getElementById('http-response')  as HTMLSelectElement).value = JSON.stringify(response, null, 2)
+      
+      // });
+
+      let obs =  this.http.post(this.path,this.data);
+        obs.subscribe(
+          data => {
+            //this.response; console.log(JSON.stringify(data));
+            (document.getElementById('http-response')  as HTMLSelectElement).value = JSON.stringify(data, null, 2)
+          },
+          error =>{
+            //this.response; console.log(JSON.stringify(error));
+            (document.getElementById('http-response')  as HTMLSelectElement).value = JSON.stringify(error, null, 2)
+
+          },
+        );
+
+      
+    }
+      catch(error){
+        console.log(error);
+      }
+
+    }   
+    else{
+      // error
+    }
+
+  }
+
+  _fill(){
+     var d = {
+        "name": "bob smith",
+        "message": "Hello World",
+        age: 26
+    };
+
+    (document.getElementById('data-input') as HTMLSelectElement).value = JSON.stringify(d, null, 2);
+  }
+
+  objToString (obj) {
+    var str = '';
+    for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            str += p + ':' + obj[p] + '\n';
+        }
+    }
+    return str;
+}
+}
